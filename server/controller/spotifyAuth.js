02e277a33d116900e128ca8eab3ebe5dc0b269ad
lemony452 <gij9808@gmail.com>
2024-03-28 18:54:46 +0900

    refactor: 파일명 변경 auth -> spotifyAuth 로 변경 자체 jwt auth 파일과 구분하기 위함

---
+++ server/controller/spotifyAuth.js
@@ -0,0 +1,107 @@
+const request = require("request");
+const querystring = require("querystring");
+const { generateRandomString } = require("../utils/utils");
+const User = require("../models/user");
+
+const client_id = process.env.client_id;
+const redirect_uri = "http://localhost:3000/callback";
+const client_secret = process.env.client_secret;
+
+// 인가코드를 요청할 주소 url 가져오기
+module.exports.getCode = (req, res) => {
+  const state = generateRandomString(16);
+  const scope = "user-read-private user-read-email";
+
+  res.json(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
+};
+
+module.exports.spotifyLogin = (req, res) => {
+  const { code, state } = req.query;
+
+  if (!code || !state)
+    return res.status(500).json("토큰을 가져오는데 실패하였습니다.");
+
+  const authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: "authorization_code",
+    },
+    json: true,
+  };
+
+  request.post(authOptions, async function (error, response, body) {
+    try {
+      if (!error && response.statusCode === 200) {
+        const tokenInfo = body;
+        const userInfo = await getUserInfo(tokenInfo.access_token);
+        const user = await User.exists({ spotifyId: userInfo.id });
+        console.log(user);
+        if (user) res.status(200).json({ tokenInfo, isSign: true, userInfo });
+        else res.status(200).json({ tokenInfo, isSign: false, userInfo });
+      } else {
+        res.status(500).json("액세스 토큰을 가져오는데 실패하였습니다");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
+
+module.exports.getRefreshToken = (req, res) => {
+  console.log("--------리프레시토큰 재발급--------");
+  const { refresh_token } = req.body;
+  const authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        new Buffer.from(
+          process.env.client_id + ":" + process.env.client_secret
+        ).toString("base64"),
+    },
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("새로 받은 토큰 정보 : ", body);
+      res.status(200).json(body);
+    } else {
+      res.status(500).json("리프레시 토큰을 가져오기 실패");
+    }
+  });
+};
+
+const getUserInfo = async (access_token) => {
+  const url = "https://api.spotify.com/v1/me";
+
+  // accesstoken으로 유저 정보 가져오기
+  const res = await fetch(url, {
+    method: "GET",
+    headers: { Authorization: "Bearer " + access_token },
+  });
+
+  return await res.json();
+};
