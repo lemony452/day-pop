b96444c41e7a10d17c4ccced91561dd710d88ca7
lemony452 <gij9808@gmail.com>
2024-03-29 14:36:24 +0900

    refactor: 로컬 서버 url env 환경 변수적용 - button 로그인 함수 분리 필요

--- src/components/btn.jsx
+++ src/components/btn.jsx
@@ -1,6 +1,21 @@
-export default function Button({ children, handleClick, ...props }) {
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Button({ children, isLogin = false, ...props }) {
+  const router = useRouter();
+  const handleLogin = async () => {
+    if (isLogin) {
+      // console.log("server url!!!: ", process.env.NEXT_PUBLIC_LOCAL_SERVER_URL);
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_LOCAL_SERVER_URL + "/code"
+      );
+      const spotifyLogin = await res.json();
+      router.push(spotifyLogin);
+    }
+  };
   return (
-    <button {...props} onClick={handleClick}>
+    <button {...props} onClick={handleLogin}>
       {children}
     </button>
   );
