7a7b529ab2a2bf076832ea0700f97d1136be8f3d
lemony452 <gij9808@gmail.com>
2024-03-29 14:24:01 +0900

    refactor: api 테스트 코드 삭제

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,25 +3,24 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Card from "@/components/card";
 import Search from "@/components/search";
+import Button from "@/components/btn";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:8080/hello");
-  const data = await res.json();
-  console.log(data);
   return (
-    // <section className={styles.welcome}>
-    //   <div>
-    //     <Image src="/pop.png" alt="pop logo" width={80} height={80} />
-    //     <p className={styles.title}>Day POP에 오신걸 환영해요!</p>
-    //     <p className={styles.subTitle}>
-    //       좋아하는 팝송을 들으며 영어 공부를 할 수 있어요 😀
-    //     </p>
-    //     <Link href="/login" className={styles.loginBtn}>
-    //       로그인
-    //     </Link>
-    //   </div>
-    // </section>
-    <Playlist></Playlist>
+    <section className={styles.welcome}>
+      <div>
+        <Image src="/pop.png" alt="pop logo" width={80} height={80} />
+        <p className={styles.title}>Day POP에 오신걸 환영해요!</p>
+        <p className={styles.subTitle}>
+          좋아하는 팝송을 들으며 영어 공부를 할 수 있어요 😀
+        </p>
+        <Button isLogin={true} className={styles.loginBtn}>
+          로그인
+        </Button>
+        {/* <SpotifyLogin className={styles.loginBtn}>로그인</SpotifyLogin> */}
+      </div>
+    </section>
+    // <Playlist></Playlist>
   );
 }
 
