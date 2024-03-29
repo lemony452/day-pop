9b7474494e2bb05e6a1b693b00ed110b6c8fdd9f
lemony452 <gij9808@gmail.com>
2024-03-29 14:19:23 +0900

    package: 서버 패키지들을 next -> server package.json으로 수정 jwt 구현을 위해 추가한 패키지 - bcrypt : 비밀번호 암호화를 위한 암호화 알고리즘 - jsonwebtoken: 토큰 발급 및 검증을 위한 모듈

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,7 +1,14 @@
 const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 
 const generateRandomString = (length) => {
   return crypto.randomBytes(60).toString("hex").slice(0, length);
 };
 
-module.exports = generateRandomString;
+const saltRounds = 10;
+
+const getHash = (value) => {
+  return bcrypt.hashSync(value, saltRounds);
+};
+
+module.exports = { generateRandomString, getHash };
