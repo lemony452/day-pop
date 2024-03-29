42282e28502b3871248ca6ba4c2abf184eb59cd5
lemony452 <gij9808@gmail.com>
2024-03-29 14:31:10 +0900

    refactor: 로그인 폼 섹션 컴포넌트로 분리 클라이언트 컴포넌트에서 폼을 사용하기 위함

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,9 @@
-import Button from "@/components/btn";
 import Header from "@/components/header";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./page.module.css";
+import LoginForm from "./loginForm";
 
-export default function LoginPage() {
+export default async function LoginPage() {
   return (
     <section className={styles["login-section"]}>
       <Header></Header>
@@ -16,29 +15,7 @@ export default function LoginPage() {
           height={80}
           priority
         ></Image>
-        <form className={styles["login-box"]}>
-          <p>
-            <label htmlFor="id">ID</label>
-            <input
-              type="text"
-              name="id"
-              id="id"
-              placeholder="아이디를 입력해주세요"
-            />
-          </p>
-          <p>
-            <label htmlFor="passwordValue">Password</label>
-            <input
-              type="password"
-              name="passwordValue"
-              id="passwordValue"
-              placeholder="비밀번호를 입력해주세요"
-              autoComplete="off"
-            />
-          </p>
-          <Button className={styles["login-btn"]}>로그인</Button>
-          <Link href="/signup">회원가입 하러 가기</Link>
-        </form>
+        <LoginForm styles={styles}></LoginForm>
       </div>
     </section>
   );
