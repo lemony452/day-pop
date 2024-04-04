af367ca0337a9ce4a6e7d0ab5dae161aa1f38629
lemony452 <gij9808@gmail.com>
2024-04-04 23:24:27 +0900

    fix: 팝송데이터를 찾는 로직 에러 수정 - 유저 모델에서 찾은 팝송리스트 결과 doc에서 findOne 사용시 오류 발생. 모델에서 사용하는 메서드임 - 팝송 모델에서 findOne을 사용해 db 조회로 수정 - res.status 에 json 응답 추가

--- server/controller/play.js
+++ server/controller/play.js
@@ -6,17 +6,27 @@ module.exports = {
   getPopsong: async (req, res) => {
     const userId = req.userId;
     const { trackId } = req.params;
+    console.log("유저 id : ", userId);
+    console.log("트랙 id : ", trackId);
 
-    const studyingList = await User.findById(userId).populate("studyingList");
-    const foundPopsong = await studyingList.findOne({ trackId });
+    // const studyingList = await User.findById(userId).populate("studyingList");
+    // console.log("내가 학습한 팝송들 : ", studyingList);
 
-    if (foundPopsong) return res.status(200).json(foundPopsong);
-    else throw new AppError(400, "학습한 적이 없는 팝송입니다");
+    const foundPopsong = await Popsong.findOne({ trackId, userId });
+
+    if (foundPopsong) res.status(200).json(foundPopsong);
+    else res.status(400).json("학습한 적이 없는 팝송입니다");
+    // throw new AppError(400, "학습한 적이 없는 팝송입니다");
   },
   addStudyingPopsong: async (req, res) => {
+    const userId = req.userId;
     const newStudyingPopsong = new Popsong(req.body);
+    newStudyingPopsong.userId = userId;
     await newStudyingPopsong.save();
-    res.status(200).json("성공적으로 저장하였습니다");
+    res.status(200).json({
+      popsongId: newStudyingPopsong._id,
+      message: "성공적으로 팝송을 저장하였습니다",
+    });
   },
   editStudyingPopsong: async (req, res) => {
     const { popsongId } = req.params;
