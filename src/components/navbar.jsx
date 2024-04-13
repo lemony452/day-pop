d165cede5624e4717965ab316b21b344dd0d3b52
lemony452 <gij9808@gmail.com>
2024-04-13 14:45:34 +0900

    refactor: 유저 정보와 학습 상태에 따라 nav 분기 처리 및 css 추가

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,78 @@
+"use client";
+
 import Image from "next/image";
 import Button from "./btn";
 import styles from "./header.module.css";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { useRouter, usePathname } from "next/navigation";
+import { savingStudyPopsong } from "@/lib/study/thunks";
+import { openModal } from "@/lib/modal/modalSlice";
+import { getCookie, setCookie } from "cookies-next";
+import { useEffect } from "react";
+import { verifyToken } from "@/lib/user/thunks";
 
 export default function Nav() {
+  const pathName = usePathname();
+  console.log(pathName);
+
+  const { studying, popsongInfo } = useAppSelector((state) => state.study);
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const ACCESS_TOKEN = getCookie("access_token");
+  const REFRESH_TOKEN = getCookie("refresh_token");
+
+  const savingData = {
+    popsongId: popsongInfo.popsongId,
+    savedData: {
+      sentenceIdx: studying.sentenceIdx,
+      studyingLyrics: studying.studyingLyrics,
+      perfect: studying.perfect,
+      great: studying.great,
+      good: studying.good,
+      bad: studying.bad,
+      miss: studying.miss,
+    },
+  };
+
+  const handleFinishStudy = () => {
+    dispatch(savingStudyPopsong(savingData));
+  };
+
+  // useEffect(() => {
+  //   dispatch(verifyToken(ACCESS_TOKEN, REFRESH_TOKEN))
+  // }, [dispatch])
+
+  // if (pathName === "/study" && studying.isDone) {
+  //   if (savingData) {
+  //     dispatch(savingStudyPopsong(savingData));
+  //     router("/")
+  //   } else router
+  // }
+
   return (
-    <>
+    <div className={styles["nav-items"]}>
+      {pathName !== "/study" && (
+        <>
+          <Link href="/myplaylist">
+            <Button className={`${styles.button} ${styles.toMain}`}>
+              플레이리스트로 돌아가기
+            </Button>
+          </Link>
+        </>
+      )}
+      {pathName === "/study" && (
+        <button
+          onClick={handleFinishStudy}
+          className={`${styles.button} ${styles.studyOver}`}
+        >
+          학습 종료
+        </button>
+      )}
       <Button className={styles.menu}>
         <Image src="/menu.png" alt="sidebar menu" width={30} height={30} />
       </Button>
-      <Link href={`/result/${1}`}>
-        <Button className={`${styles.button} ${styles.studyOver}`}>
-          학습 종료
-        </Button>
-      </Link>
-      <Link href="/">
-        <Button className={`${styles.button} ${styles.toMain}`}>
-          메인으로 돌아가기
-        </Button>
-      </Link>
-    </>
+    </div>
   );
 }
