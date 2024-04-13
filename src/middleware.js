fffdd15c036646490db7853ce3b783eeb254907d
lemony452 <gij9808@gmail.com>
2024-04-13 14:04:21 +0900

    fix: 토큰 재발급 후 cookies-next의 setCookie 적용 안되는 로직 삭제

--- src/middleware.js
+++ src/middleware.js
@@ -1,75 +1,54 @@
 import { getRefresh, getSpotifyRefresh } from "@/lib/auth";
 import { getPlayState, transferPlayer } from "@/lib/play";
-import { getCookies } from "cookies-next";
+import { getCookies, setCookie } from "cookies-next";
 import { NextResponse } from "next/server";
 
-// import { cookies } from "next/headers";
-
 export async function middleware(request, event) {
+  const url = request.nextUrl.origin;
+  // console.log(request.nextUrl);
   let spotify_access_token;
+  console.log("cookies-next : ", getCookies());
   const { cookies } = request;
+  const response = NextResponse.next();
   console.log("쿠키들: ", cookies.getAll());
 
-  const access_token = cookies.get("access_token");
-  const refresh_token = cookies.get("refresh_token");
-  const spotify_refresh_token = cookies.get("spotify_refresh_token");
+  const access_token = cookies.get("access_token").value;
+  const refresh_token = cookies.get("refresh_token").value;
+  const spotify_refresh_token = cookies.get("spotify_refresh_token").value;
 
   if (!spotify_refresh_token && request.nextUrl.pathname !== "/") {
     console.log("스포티파이 로그인 필요");
-    return NextResponse.redirect(new URL("/"));
+    return NextResponse.redirect(new URL(url));
   } else {
     console.log("스포티파이 액세스 토큰 가져오기");
     const tokenInfo = await getSpotifyRefresh(spotify_refresh_token);
     if (tokenInfo) spotify_access_token = tokenInfo.access_token;
   }
 
-  if (!access_token || !refresh_token) {
+  if (spotify_refresh_token && (!access_token || !refresh_token)) {
     console.log("day-pop 로그인 필요");
-    return NextResponse.redirect(new URL("/login"));
+    return NextResponse.redirect(new URL(url + "/login", request.url));
   }
 
   if (request.nextUrl.pathname.startsWith("/myplaylist")) {
     console.log("내 플레이리스트에 옴");
-    const { device } = await getPlayState(spotify_access_token);
-    console.log("내 디바이스 정보 : ", device);
-    if (device && !device.is_active) {
-      console.log("디바이스 전환 필요");
-      await transferPlayer(spotify_access_token, device.id);
-    }
-    return NextResponse.rewrite(new URL("/myplaylist", request.url));
+    // const { device } = await getPlayState(spotify_access_token);
+    // console.log("내 디바이스 정보 : ", device);
+    // if (device && !device.is_active) {
+    // }
+    // console.log("디바이스 전환 필요");
+    // await transferPlayer(spotify_access_token, device.id);
+    return NextResponse.rewrite(new URL(url + "/myplaylist"));
   }
 
-  // event.waitUntil(
-  //   fetch(`http://localhost:8080/api/refresh}`, {
-  //     method: "POST",
-  //     headers: {
-  //       Authorizaiton: "Bearer " + access_token,
-  //       refreshToken: refresh_token,
-  //     },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((tokenInfo) => {
-  //       console.log(tokenInfo);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     })
-  // getRefresh(access_token, refresh_token)
-  //   .then((res) => {
-  //     console.log("미들웨어 요청 결과 : ", res);
-  //     const { new_access_token, new_refresh_token } = res;
-  //     cookies.set("access_token", new_access_token);
-  //     cookies.set("refresh_token", new_refresh_token);
-  //   })
-  //   .catch((err) => {
-  //     if (err.status === 401) return NextResponse.redirect(new URL("/login"));
-  //     console.log(err);
-  //   })
-  // );
-
-  return NextResponse.next();
+  return response;
 }
 
 export const config = {
-  matcher: ["/myplaylist"],
+  matcher: [
+    "/myplaylist/:path",
+    "/detail/:path",
+    "/study/:path",
+    "/result/:path",
+  ],
 };
