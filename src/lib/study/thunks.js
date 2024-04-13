b8c322bd17f2b3508032e976056fffce0e85ca31
lemony452 <gij9808@gmail.com>
2024-04-13 23:34:36 +0900

    refactor: return-fetch 적용 및 에러 처리

--- src/lib/study/thunks.js
+++ src/lib/study/thunks.js
@@ -1,15 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { addStudyingList, getPopsongInfo, editStudyingPopsong } from "../play";
+import {
+  addStudyingList,
+  getPopsongInfo,
+  editStudyingPopsong,
+  fethcPopsongExtended,
+} from "../play";
 import { studyActions } from "./studySlice";
 
 // 팝송 데이터 가져오기
+// return-fetch 적용
 export const fetchPopsongById = createAsyncThunk(
   "study/fetchPopsongStatus", // thunk 함수 이름
   async (trackId, { rejectWithValue }) => {
     // 비동기 payloadCreator 함수 정의
     try {
-      const res = await getPopsongInfo(trackId);
-      return res;
+      const res = await fethcPopsongExtended(`/api/popsong/${trackId}`);
+      if (!res.ok) throw Error(res.status);
+      return await res.json();
     } catch (e) {
       console.log(e);
       return rejectWithValue(e.message);
@@ -17,33 +24,20 @@ export const fetchPopsongById = createAsyncThunk(
   }
 );
 
-// export const fetchPopsongInfo = (trackId, access_token) => {
-//   return async (dispatch) => {
-//     const popsongInfo = await getPopsongInfo(trackId, access_token);
-//     console.log("팝송 있는지 확인 : ", popsongInfo);
-//     if (!popsongInfo) console.log("팝송을 학습한 정보를 가져올 수 없습니다");
-//     else {
-//       const data = {
-//         title: popsongInfo.title,
-//         artist: popsongInfo.artist,
-//         originalLyrics: popsongInfo.originalLyrics,
-//         totalSentence: popsongInfo.originalLyrics.length,
-//       };
-//       dispatch(studyActions.selectPopsong(data));
-//     }
-//   };
-// };
-
 // 팝송 학습 정보 첫 등록하기
 export const addStudyPopsong = createAsyncThunk(
   "study/addPopsongStatus",
   async (popsongData, { rejectWithValue }) => {
     try {
-      const res = await addStudyingList(popsongData);
-      return res;
+      const res = await fethcPopsongExtended("/api/popsong", {
+        method: "POST",
+        body: JSON.stringify(popsongData),
+      });
+      if (!res.ok) throw Error(res.status);
+      return await res.json();
     } catch (e) {
       console.log(e);
-      return rejectWithValue(e.Error);
+      return rejectWithValue(e.message);
     }
   }
 );
@@ -52,12 +46,28 @@ export const addStudyPopsong = createAsyncThunk(
 export const savingStudyPopsong = createAsyncThunk(
   "study/savingPopsongStatus",
   async (savingData, { rejectWithValue }) => {
+    const bodyValue = {
+      savedData: savingData.savedData,
+    };
+    if (savingData.grade && savingData.score) {
+      Object.assign(bodyValue, {
+        score: savingData.score,
+        grade: savingData.grade,
+      });
+    }
     try {
-      const res = await editStudyingPopsong(savingData);
-      return res;
+      const res = await fethcPopsongExtended(
+        `/api/popsong/${savingData.popsongId}`,
+        {
+          method: "PATCH",
+          body: JSON.stringify(bodyValue),
+        }
+      );
+      if (!res.ok) throw Error(res.status);
+      return await res.json();
     } catch (e) {
       console.log(e);
-      return rejectWithValue(e.Error);
+      return rejectWithValue(e.message);
     }
   }
 );
