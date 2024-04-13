39e77df7834e49539ca8be49dfae2aff947446ac
lemony452 <gij9808@gmail.com>
2024-04-13 14:46:14 +0900

    refactor: 카드 컴포넌트 props 값으로 적용

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,18 @@
+import Image from "next/image";
 import styles from "./card.module.css";
 
-export default function Card({ imgSize }) {
+export default function Card({ imgSize, cardInfo }) {
   return (
     <div className={styles.cardWrapper}>
-      <div
+      <Image
+        src={cardInfo.trackImgSrc}
+        alt="album image"
         className={styles.image}
-        style={{ width: `${imgSize}px`, height: `${imgSize}px` }}
-      ></div>
-      <div className={styles.title}>Title</div>
-      <div className={styles.artist}>Artist</div>
+        width={imgSize}
+        height={imgSize}
+      ></Image>
+      <div className={styles.title}>{cardInfo.trackTitle}</div>
+      <div className={styles.artist}>{cardInfo.trackArtist}</div>
     </div>
   );
 }
