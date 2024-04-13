3eee8268cd4960648d4beae800c194aedb34b855
lemony452 <gij9808@gmail.com>
2024-04-13 23:11:59 +0900

    refactor: 토큰 검증 로직 및 쿠키 삭제 - 팝송 데이터를 가져오는 로직에 return-fetch 라이브러리를 적용하면서 토큰 검증을 따로 할 필요 없어짐

--- src/app/detail/result.jsx
+++ src/app/detail/result.jsx
@@ -1,46 +1,14 @@
 "use client";
 
-import { getRefresh } from "@/lib/auth";
 import { useAppSelector, useAppDispatch } from "@/lib/hooks";
 import { fetchPopsongById } from "@/lib/study/thunks";
-import { verifyToken } from "@/lib/user/thunks";
-import { getCookie, setCookie } from "cookies-next";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 let isRender = false;
-const ACCESS_TOKEN = getCookie("access_token");
-const REFRESH_TOKEN = getCookie("refresh_token");
 
 export default function Result({ styles, trackId }) {
-  // const [accessToken, setAccessToken] = useState(ACCESS_TOKEN);
-
-  const router = useRouter();
   const dispatch = useAppDispatch();
   const { result } = useAppSelector((state) => state.study);
-  const { access_token, refresh_token } = useAppSelector((state) => state.user);
-
-  // const verifyToken = async () => {
-  //   console.log("토큰 검증");
-  //   try {
-  //     const res = await getRefresh(accessToken, REFRESH_TOKEN);
-  //     if (!res.ok) {
-  //       if (res.status === 401) return router("/login");
-  //       else if (res.status !== 400) throw new Error("에러가 발생하였습니다");
-  //     }
-  //     if (res.status === 200) {
-  //       const newTokenInfo = await res.json();
-  //       setCookie("access_token", newTokenInfo.access_token);
-  //       setAccessToken(newTokenInfo.access_token);
-  //     }
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   dispatch(verifyToken(ACCESS_TOKEN, REFRESH_TOKEN))
-  // }, [dispatch])
 
   useEffect(() => {
     if (!isRender) {
