c91ba6510661a02703af4fa385b6397b01901dcf
lemony452 <gij9808@gmail.com>
2024-04-13 13:58:10 +0900

    refactor: api 라우트 변경

--- server/routes/play.js
+++ server/routes/play.js
@@ -8,10 +8,9 @@ const authJWT = require("../utils/middleware");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .post(authJWT, addStudyingPopsong)
-  .patch(authJWT, editStudyingPopsong);
+router.route("/").post(authJWT, addStudyingPopsong);
+
+router.route("/:popsongId").patch(authJWT, editStudyingPopsong);
 
 router.route("/:trackId").get(authJWT, getPopsong);
 
