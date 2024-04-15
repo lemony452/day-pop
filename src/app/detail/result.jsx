1230031ee2653a36e6f7bad4bbf132db90e82209
lemony452 <gij9808@gmail.com>
2024-04-15 23:57:14 +0900

    refactor: 학습 상태에 따라 진행률 및 Rank 조건 추가

--- src/app/detail/result.jsx
+++ src/app/detail/result.jsx
@@ -8,16 +8,30 @@ let isRender = false;
 
 export default function Result({ styles, trackId }) {
   const dispatch = useAppDispatch();
-  const { result } = useAppSelector((state) => state.study);
+  const { isStudy, result, studying, popsongInfo } = useAppSelector(
+    (state) => state.study
+  );
+  const progress = Math.ceil(
+    (studying.sentenceIdx / popsongInfo.originalLyrics.length) * 100
+  );
 
   useEffect(() => {
     if (!isRender) {
       isRender = true;
-      return;
+    } else {
+      console.log("팝송 정보 가져오기");
+      dispatch(fetchPopsongById(trackId));
     }
-
-    dispatch(fetchPopsongById(trackId));
   }, [trackId, dispatch]);
 
-  return <div className={styles["result-grade"]}>{result.grade}</div>;
+  return (
+    <div className={styles["result-grade"]}>
+      <div className={styles["result-info"]}>
+        {isStudy && result.grade === "-" ? "진행률" : "Rank"}
+      </div>
+      <div className={styles["result"]}>
+        {isStudy && result.grade === "-" ? `${progress}%` : result.grade}
+      </div>
+    </div>
+  );
 }
