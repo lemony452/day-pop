5610909cf495d31421d2dee2941d5f986eed380f
lemony452 <gij9808@gmail.com>
2024-04-16 20:11:53 +0900

    refactor: 팝송 학습 히스토리 배열에 추가 및 평균 점수&랭크 데이터 저장

--- server/controller/play.js
+++ server/controller/play.js
@@ -21,19 +21,46 @@ module.exports = {
   },
   addStudyingPopsong: async (req, res) => {
     const userId = req.userId;
+    const foundUser = await User.findById(userId);
+
     const newStudyingPopsong = new Popsong(req.body);
     newStudyingPopsong.userId = userId;
+
+    foundUser.studyingList.push(newStudyingPopsong._id); // 팝송 학습 시 히스토리에 기록
+    await foundUser.save();
     await newStudyingPopsong.save();
+
     res.status(200).json({
       popsongId: newStudyingPopsong._id,
       message: "성공적으로 팝송을 저장하였습니다",
     });
   },
   editStudyingPopsong: async (req, res) => {
+    const userId = req.userId;
     const { popsongId } = req.params;
     console.log(popsongId);
     console.log(req.body);
     const reqData = req.body;
+
+    if (reqData.score && reqData.grade) {
+      const foundUser = await User.findById(userId);
+      const newScore = foundUser.score + reqData.score; // 새로 학습한 점수 누적
+      const avgScore = newScore / 2; // 평균 점수
+
+      let avgGrade = foundUser.grade; // 평균 랭크
+      if (avgScore >= 10) avgGrade = "S";
+      else if (avgScore >= 8) avgGrade = "A+";
+      else if (avgScore >= 7) avgGrade = "A";
+      else if (avgScore >= 6) avgGrade = "B";
+      else if (avgScore >= 5) avgGrade = "C";
+      else if (avgScore >= 4) avgGrade = "D";
+      else avgGrade = "F";
+
+      foundUser.score = newScore;
+      foundUser.grade = avgGrade;
+
+      await foundUser.save();
+    }
 
     const updatePopsong = await Popsong.findByIdAndUpdate(popsongId, reqData, {
       runValidators: true,
