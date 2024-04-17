e56cadafb6590ca84a62efada3e9b48554a6c019
lemony452 <gij9808@gmail.com>
2024-04-17 09:56:16 +0900

    refactor: 로그인 유무에 따라 Nav 컴포넌트 노출 조정 - 로그인 유무 상태관리를 위해 클라이언트 컴포넌트로 변경 - layout에서 prerendering 이후 hydration 할 때 액세스 토큰 유무 판단

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./header.module.css";
 import { fat } from "@/app/layout";
 import Nav from "./navbar";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { getCookie } from "cookies-next";
+import { userActions } from "@/lib/user/userSlice";
 
 export default function Header() {
-  return (
-    <header className={`${styles.header} ${fat.variable}`}>
-      <div className={styles.wraper}>
-        <Link href="/" className={styles.logo}>
-          Day POP
-        </Link>
-        <Nav />
-      </div>
-    </header>
-  );
+  const [isClient, setIsClient] = useState(false);
+  const { isLogin } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    setIsClient(true);
+    if (getCookie("access_token")) dispatch(userActions.login());
+  }, [dispatch]);
+
+  if (isClient) {
+    return (
+      <header className={`${styles.header} ${fat.variable}`}>
+        <div className={styles.wraper}>
+          <Link href="/" className={styles.logo}>
+            Day POP
+          </Link>
+          {isLogin && <Nav />}
+        </div>
+      </header>
+    );
+  } else {
+    return <></>;
+  }
 }
