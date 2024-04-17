5ad422e507c9f93a09c237e8dd124b837b28f9d6
lemony452 <gij9808@gmail.com>
2024-04-17 09:47:59 +0900

    fix: 쿠키 값이 없는 경우 value 메소드 에러 해결 - ? 추가

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { getPlayState, transferPlayer } from "@/lib/play";
 import { getCookies, setCookie } from "cookies-next";
 import { NextResponse } from "next/server";
 
-export async function middleware(request, event) {
+export async function middleware(request) {
   const url = request.nextUrl.origin;
   // console.log(request.nextUrl);
   let spotify_access_token;
@@ -12,9 +12,9 @@ export async function middleware(request, event) {
   const response = NextResponse.next();
   console.log("쿠키들: ", cookies.getAll());
 
-  const access_token = cookies.get("access_token").value;
-  const refresh_token = cookies.get("refresh_token").value;
-  const spotify_refresh_token = cookies.get("spotify_refresh_token").value;
+  const access_token = cookies.get("access_token")?.value;
+  const refresh_token = cookies.get("refresh_token")?.value;
+  const spotify_refresh_token = cookies.get("spotify_refresh_token")?.value;
 
   if (!spotify_refresh_token && request.nextUrl.pathname !== "/") {
     console.log("스포티파이 로그인 필요");
@@ -28,17 +28,6 @@ export async function middleware(request, event) {
   if (spotify_refresh_token && (!access_token || !refresh_token)) {
     console.log("day-pop 로그인 필요");
     return NextResponse.redirect(new URL(url + "/login", request.url));
-  }
-
-  if (request.nextUrl.pathname.startsWith("/myplaylist")) {
-    console.log("내 플레이리스트에 옴");
-    // const { device } = await getPlayState(spotify_access_token);
-    // console.log("내 디바이스 정보 : ", device);
-    // if (device && !device.is_active) {
-    // }
-    // console.log("디바이스 전환 필요");
-    // await transferPlayer(spotify_access_token, device.id);
-    return NextResponse.rewrite(new URL(url + "/myplaylist"));
   }
 
   return response;
