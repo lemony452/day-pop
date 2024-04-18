9e4602187908a41f0369472e8a7ebf574c622b8c
lemony452 <gij9808@gmail.com>
2024-04-18 15:03:24 +0900

    refactor: loading 페이지에 자식 컴포넌트를 props로 추가

--- client/src/app/dashboard/loading.jsx
+++ client/src/app/dashboard/loading.jsx
@@ -1,11 +1,12 @@
 import styles from "./loading.module.css";
 
-export default function Loading() {
+export default function Loading({ children }) {
   return (
     <section className={styles.loading}>
       <div className={styles.loader}></div>
       <div className={styles.loader}></div>
       <div className={styles.loader}></div>
+      {children}
     </section>
   );
 }
