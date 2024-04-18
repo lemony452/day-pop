d1fd64053421b1006f70d6dda456fbdcdc240aa9
lemony452 <gij9808@gmail.com>
2024-04-18 19:55:24 +0900

    refactor: mongoDB server url로 변경

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
+const LOCAL_MONGODB_URL = "mongodb://127.0.0.1:27017/day-pop";
+const MONGODB_URL = process.env.MONGODB_URL;
+
 const connectServer = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/day-pop")
+    .connect(MONGODB_URL)
     .then(() => {
       console.log("Mongoose Connected");
     })
