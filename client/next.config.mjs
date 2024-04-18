53b405101325519328d2b5427f6648b0ef0e006b
lemony452 <gij9808@gmail.com>
2024-04-18 15:06:56 +0900

    feat: webpack 빌드 시 @/lib 경로 모듈을 찾을 수 있도록 config 추가 - 빌드 시 해당 @/lib 경로를 찾지 못해 모듈을 가져오지 못하는 에러 발생

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+import path from "path";
+const __dirname = new URL(".", import.meta.url).pathname;
+
 const nextConfig = {
   reactStrictMode: false,
   images: {
@@ -12,6 +16,14 @@ const nextConfig = {
         hostname: "i.scdn.co",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./src"),
+    };
+
+    return config;
   },
 };
 
